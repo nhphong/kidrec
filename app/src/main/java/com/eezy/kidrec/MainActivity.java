7d5a4b1e8f1eab16632773f0d32dda863ae89cb1
Phong Nguyen <phong.nguyen@eastagile.com>
2016-04-13 22:50:14 +0700

    first commit

---
+++ app/src/main/java/com/eezy/kidrec/MainActivity.java
@@ -0,0 +1,13 @@
+package com.eezy.kidrec;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+public class MainActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
